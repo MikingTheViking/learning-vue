ac48e3ad5bec53b505018574efbbe417fc36f785
MikingTheViking <kingmi123@gmail.com>
2017-10-29 17:00:32 -0400

    Browsersync added

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -13,3 +13,22 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+
+mix.autoload({
+	jquery: ['$', 'global.jQuery', 'jQuery', 'global.$', 'jquery', 'global.jquery']
+});
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+mix.webpackConfig({
+	plugins: [
+	new BrowserSyncPlugin({
+		files: [
+			'app/**/*',
+			'public/**/*',
+			'resources/views/**/*',
+			'routes/**/*'
+		]
+	})]
+});
