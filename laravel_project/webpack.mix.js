239e5cf85fac81b29d1a61c1303feae19bd6ec91
Unknown <kingmi123@gmail.com>
2018-02-08 21:23:32 -0500

    Moved store location, tracking correct learnvue script

--- laravel_project/webpack.mix.js
+++ laravel_project/webpack.mix.js
@@ -6,7 +6,7 @@ mix
 	.sass('resources/assets/sass/welcome.scss', 'public/css')
 	//.js('resources/assets/js/fontawesome-5-demo', 'public/js')
 	//.sass('resources/assets/sass/fontawesome-5.scss', 'public/css')
-	.js('resources/assets/js/learnvue.js', 'public/js')
+	.js('resources/assets/js/learnvue/index.js', 'public/js')
 	// .js('resources/assets/js/learn-es2015.js', 'public/js')
 	.js('resources/assets/js/app.js', 'public/js')
    	.sass('resources/assets/sass/app.scss', 'public/css');
